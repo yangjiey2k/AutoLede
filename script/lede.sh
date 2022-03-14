89ba2b23eba88eea962b79f49aa128f47b7d932f
yangjiey2k <42866354+yangjiey2k@users.noreply.github.com>
2022-03-14 22:52:22 +0800

    Update lede.sh

--- script/lede.sh
+++ script/lede.sh
@@ -22,7 +22,7 @@ popd
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.202/g' package/base-files/files/bin/config_generate
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='小杰的私人定制固件'' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='小杰私人定制'' package/lean/default-settings/files/zzz-default-settings
 sed -i "s/OpenWrt /YJ @ KoNanWrt /g" package/lean/default-settings/files/zzz-default-settings
 # find package/*/ feeds/*/ -maxdepth 6 -path "*luci-app-smartdns/luasrc/controller/smartdns.lua" | xargs -i sed -i 's/\"SmartDNS\")\, 4/\"SmartDNS\")\, 3/g' {} 
 # Test kernel 5.15
