78de4c8a72cf8d2e2a7ae02cd84a9e42471c5059
yangjiey2k <42866354+yangjiey2k@users.noreply.github.com>
2023-08-02 20:46:24 +0800

    Update hook-feeds.sh

--- script/hook-feeds.sh
+++ script/hook-feeds.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-# pushd customfeeds
-
-# Add luci-app-netdata
-# rm -rf packages/admin/netdata
-# svn co https://github.com/281677160/openwrt-package/trunk/feeds/packages/net/netdata packages/admin/netdata
-# rm -rf ../package/lean/luci-app-netdata
-# svn co https://github.com/281677160/openwrt-package/trunk/feeds/luci/applications/luci-app-netdata luci/applications/luci-app-netdata
-
-# popd
 
 # Set to local feeds
 pushd customfeeds/packages
@@ -16,14 +7,14 @@ popd
 pushd customfeeds/luci
 export luci_feed="$(pwd)"
 popd
+
+# Add custom feeds to feeds.conf.default
 sed -i '/src-git packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
 sed -i '/src-git luci/d' feeds.conf.default
 echo "src-link luci $luci_feed" >> feeds.conf.default
 sed -i '/helloworld/d' feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
-
-
 
 # Update feeds
 ./scripts/feeds update -a
