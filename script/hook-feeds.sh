c1fa4a5e6f9163e26f551758b100de112dcc5c83
yangjiey2k <42866354+yangjiey2k@users.noreply.github.com>
2023-08-02 21:26:07 +0800

    Update hook-feeds.sh

--- script/hook-feeds.sh
+++ script/hook-feeds.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Set to local feeds
-pushd customfeeds/packages
-export packages_feed="$(pwd)"
-popd
-pushd customfeeds/luci
-export luci_feed="$(pwd)"
-popd
+pushd customfeeds/packages > /dev/null
+packages_feed="$(pwd)"
+popd > /dev/null
+
+pushd customfeeds/luci > /dev/null
+luci_feed="$(pwd)"
+popd > /dev/null
 
 # Add custom feeds to feeds.conf.default
 sed -i '/src-git packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
+
 sed -i '/src-git luci/d' feeds.conf.default
 echo "src-link luci $luci_feed" >> feeds.conf.default
+
 sed -i '/helloworld/d' feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
 
