bb4a5855ba57bd3f679309dbd52bdd72d0f31168
yangjiey2k <42866354+yangjiey2k@users.noreply.github.com>
2024-07-20 09:11:44 +0800

    Update ledeRouter.sh

--- script/ledeRouter.sh
+++ script/ledeRouter.sh
@@ -28,6 +28,9 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-a
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 # git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
+# AdGuardHome 插件
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
